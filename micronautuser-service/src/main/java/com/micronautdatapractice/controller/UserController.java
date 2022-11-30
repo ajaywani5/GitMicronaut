16465fd34fa54f57fc4dda57cfe1ac8b5f73993e
Ajay Wani <ajay.wani1986@gmail.com>
2022-11-30 10:45:40 +0530

    Add third comment on delete request in UserController class

--- micronautuser-service/src/main/java/com/micronautdatapractice/controller/UserController.java
+++ micronautuser-service/src/main/java/com/micronautdatapractice/controller/UserController.java
@@ -34,7 +34,7 @@ public class UserController {
     public HttpResponse<User> updateUser(@PathVariable int id,@Body User user){
         return HttpResponse.ok(userService.updateUser(id,user));
     }
-
+// comment added on delete request
     @Delete("/{id}")
     public HttpResponse<Void> deleteUser(@PathVariable int id){
         userService.deleteUser(id);
